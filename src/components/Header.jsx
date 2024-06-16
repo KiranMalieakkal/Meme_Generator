25deb8d04c38235bbe35bb8882861ba5758738c9
Kiran Malieakkal <kiranmalieakkal18@gmail.com>
2024-06-16 15:32:04 +0200

    setting up the project and adding the Header Component.

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+const Header = () => {
+  return (
+    <header className="header">
+      <img
+        src="./assets/troll-face.png"
+        className="header--image"
+        alt="No Image"
+      />
+      <h2 className="header--title">Meme Generator</h2>
+      <h4 className="header--project">Learn React-Kiran</h4>
+    </header>
+  );
+};
+
+export default Header;
